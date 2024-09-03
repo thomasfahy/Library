2469f10ce68bd962b1bf266a29783ceacfd1d51d
thomasfahy <thomasfahy11@gmail.com>
2024-09-03 20:29:38 +0100

    feat: add constructor for book object

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 const myLibrary = [];
 
-function Book() {
-  // the constructor...
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+
+  function isRead(){
+    this.read != this.read;
+  }
+
+  function bookInfo(){
+    return(this.title,this.author,this.pages,this.read);
+  }
+
 }
 
 function addBookToLibrary() {
