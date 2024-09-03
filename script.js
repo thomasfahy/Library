8934d56a63b6bb30c8b5c8ff1815d11b84e27f42
thomasfahy <thomasfahy11@gmail.com>
2024-09-03 21:08:35 +0100

    feat: initial basic javascript build implemented

--- script.js
+++ script.js
@@ -1,21 +1,37 @@
+//Initialize library array
+
 const myLibrary = [];
+
+//Create initial books
+
+let book1 = new Book("Harry Potter", "JK Rowling", 435, true);
+let book2 = new Book("The Awakening", "L.J Smith", 256, false);
+let book3 = new Book("The Struggle", "L.J Smith", 304, false);
+let book4 = new Book("The Fury", "L.J Smith", 256, false);
+
+//Book object initialized
 
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
-  this.read = read;
+  this.read = read ? 'read' : 'not read yet';
 
-  function isRead(){
-    this.read != this.read;
+  this.toggleRead = function(){
+    this.read = !this.read;
   }
 
-  function bookInfo(){
+  this.bookInfo = function(){
     return(this.title,this.author,this.pages,this.read);
   }
-
 }
 
 function addBookToLibrary() {
-  // do stuff here
-}
+    // do stuff here
+  }
+
+
+
+
+
+console.log(harryPotter.bookInfo());
